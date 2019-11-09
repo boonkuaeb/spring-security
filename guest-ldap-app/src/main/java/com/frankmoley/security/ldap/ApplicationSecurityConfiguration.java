8dba55034bcbdcd771737e97a81fde74d644d691
BOONKUAE BOONSUTTA <boonkuae.boonsutta@scb.co.th>
2019-11-09 21:58:30 +0700

    Implementing LDAP for authentication

--- guest-ldap-app/src/main/java/com/frankmoley/security/ldap/ApplicationSecurityConfiguration.java
+++ guest-ldap-app/src/main/java/com/frankmoley/security/ldap/ApplicationSecurityConfiguration.java
@@ -1,10 +1,7 @@
 package com.frankmoley.security.ldap;
 
-import com.frankmoley.security.ldap.auth.LandonUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,24 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter{
-    @Autowired
-    private LandonUserDetailsService userDetailsService;
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
-        provider.setAuthoritiesMapper(authoritiesMapper());
-        return provider;
-    }
 
     @Bean
     public GrantedAuthoritiesMapper authoritiesMapper()
@@ -40,9 +26,20 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         return authorityMapper;
     }
 
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .authoritiesMapper(authoritiesMapper())
+                .contextSource()
+                .url("ldap://localhost:8389/dc=frankmoley,dc=com")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
 
     @Override
